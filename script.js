c5ba2cf86b58c2101fb3ea51f729422bafe54f44
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2025-05-08 13:52:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const isPowerOfTwo = (number) => {
-  //your code here
+  // A power of two is greater than 0 and has exactly one bit set in binary.
+  // So number & (number - 1) === 0 means it's a power of 2
+  return number > 0 && (number & (number - 1)) === 0;
 };
 
 const num = parseInt(prompt("Enter a number"));
